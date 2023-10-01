d458d98379697821b763cb6220325c98460175a8
Taylor Redden <tjredden1@gmail.com>
2023-09-30 20:08:06 -0700

    machine1

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,41 +4,83 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+public struct Factori {
+    public Vector3Int position;
+    public string type;
+}
+
+[Serializable]
+public struct FacType{
+    public string key;
+    public Tile val;
+}
 public class PlayerController : MonoBehaviour
 {
     public Tilemap tilemap;
+    public Tilemap factorymap;
+    public Tilemap factoryui;
     public Tile black;
     public GameObject camera;
     private int spacePress;
     private IEnumerator<Vector3Int> dit;
     private int maxx, maxy, minx, miny;
     
+    List<Factori> factories;
+    public List<FacType> facTypes;
+    Dictionary<string,Tile> factoryTypes;
+    Vector3Int? previousTile;
     // Start is called before the first frame update
     void Start()
     {
+        factoryTypes = new Dictionary<string,Tile>{};
+        foreach (FacType item in facTypes)
+            factoryTypes.Add(item.key,item.val);
+        //Debug.Log(factoryTypes.Keys);
         Debug.Log("gamestart");
         dit = GenDiamond().GetEnumerator();
         maxx=0; maxy=0; minx=0; miny=0;
         spacePress = 0;
+        previousTile = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space)){
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+        Vector3Int cellPosition = factoryui.WorldToCell(worldPosition);
+        Debug.Log(factoryTypes.Keys);
+        if(previousTile.HasValue){
+            factoryui.SetTile(previousTile.Value,null);
+        }
+        factoryui.SetTile(cellPosition,factoryTypes["liner"]);
+        previousTile = cellPosition;
+
+        if(Input.GetMouseButtonDown(0)){
+            factories.Add(new Factori{position=cellPosition,type="liner"});
+            factorymap.SetTile(cellPosition,factoryTypes["liner"]);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
             Debug.Log("spacebar: " + spacePress);
             dit.MoveNext();
             Debug.Log(dit.Current);
-            int currx=dit.Current.x,curry=dit.Current.y;
-            maxx=Math.Max(maxx,currx);
-            maxy=Math.Max(maxy,curry);
-            minx=Math.Min(minx,currx);
-            miny=Math.Min(miny,curry);
-            AdjustCamera();
-            tilemap.SetTile(dit.Current,black);
-            spacePress+=1;
-            
+            MakeSpace(dit.Current);
+            spacePress+=1;            
         }
+
+        
+    }
+
+    void MakeSpace(Vector3Int place){
+        int currx=place.x,curry=place.y;
+        maxx=Math.Max(maxx,currx);
+        maxy=Math.Max(maxy,curry);
+        minx=Math.Min(minx,currx);
+        miny=Math.Min(miny,curry);
+        AdjustCamera();
+        tilemap.SetTile(place,black);
     }
 
     void AdjustCamera(){
@@ -60,18 +102,10 @@ public class PlayerController : MonoBehaviour
         int x=0,y=0;
         while(true){
             ring++;x=0;y=ring;
-            for(int i=0;i<ring;i++){
-                yield return new Vector3Int(x++,y--);
-            }
-            for(int i=0;i<ring;i++){
-                yield return new Vector3Int(x--,y--);
-            }
-            for(int i=0;i<ring;i++){
-                yield return new Vector3Int(x--,y++);
-            }
-            for(int i=0;i<ring;i++){
-                yield return new Vector3Int(x++,y++);
-            }
+            for(int i=0;i<ring;i++) yield return new Vector3Int(x++,y--);
+            for(int i=0;i<ring;i++) yield return new Vector3Int(x--,y--);
+            for(int i=0;i<ring;i++) yield return new Vector3Int(x--,y++);
+            for(int i=0;i<ring;i++) yield return new Vector3Int(x++,y++);
         }
     }
 }
